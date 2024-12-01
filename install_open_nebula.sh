28826c8b4245979645e959c1df04f97ef13eb0f3
Jhordy Barrera <info@jhordybarrera.dev>
2024-12-01 18:27:26 -0500

    Update install_open_nebula.sh

--- install_open_nebula.sh
+++ install_open_nebula.sh
@@ -10,10 +10,13 @@ whoami
 ubuntu_version=$(lsb_release -sr)
 
 # Actualizar repositorios de ubuntu
-apt-get update
+apt update
+
+# Actualizar paquetes de ubuntu
+apt upgrade -y
 
 # Instalar paquetes necesarios
-apt-get -y install gnupg wget apt-transport-https
+apt -y install gnupg wget apt-transport-https
 
 # Agregar llave de acceso a repositorios de OpenNebula
 wget -q -O- https://downloads.opennebula.io/repo/repo2.key | gpg --dearmor --yes --output /etc/apt/keyrings/opennebula.gpg
@@ -22,22 +25,23 @@ wget -q -O- https://downloads.opennebula.io/repo/repo2.key | gpg --dearmor --yes
 echo "deb [signed-by=/etc/apt/keyrings/opennebula.gpg] https://downloads.opennebula.io/repo/6.10/Ubuntu/$ubuntu_version stable opennebula" > /etc/apt/sources.list.d/opennebula.list
 
 # Actualizar repositorios con paquetes de OpenNebula agregados arriba
-apt-get update
+apt update
 
 # Instalar paquetes de OpenNebula y servicios
-apt-get -y install opennebula opennebula-fireedge opennebula-gate opennebula-flow opennebula-provision
+apt -y install opennebula opennebula-fireedge opennebula-gate opennebula-flow opennebula-provision
 
 # Cambiar contraseña de oneadmin con la estructura usuario:contraseña o dejar comentado para usar la contraseña autogenerada
 #echo 'oneadmin:miContraseña123' > /var/lib/one/.one/one_auth
 
-# Iniciar los servicios de OpenNebula
-systemctl start opennebula opennebula-fireedge opennebula-gate opennebula-flow
+# Habilitar e iniciar los servicios de OpenNebula
+systemctl enable --now opennebula opennebula-fireedge opennebula-gate opennebula-flow
 
 # Verificar la instalación
 oneuser show
 
-# Mostrar la url de acceso
-echo "Inicia sesión en: http://127.0.0.1:2616/fireedge/sunstone"
+# Mostrar las posibles urls de acceso
+echo "Inicia sesión en alguna de las siguientes direcciones:"
+ip -o -4 addr show | awk '{print "https://" substr($4, 1, index($4, "/")-1) ":8006/fireedge/sunstone"}'
 
 # Obtener el usuario y contraseña
 user_pass=$(cat /var/lib/one/.one/one_auth)
@@ -47,3 +51,9 @@ echo "Usuario y contraseña: $user_pass"
 
 # Instalar herramientas de red
 apt install net-tools
+
+# Instalar herramienta de verificación de virtualización
+apt install cpu-checker -y
+kvm-ok
+
+
